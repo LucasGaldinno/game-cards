99a291d9564ca67d06215e53a922f12c67c9a901
Lucas Galdino <71888039+LucasGaldinno@users.noreply.github.com>
2021-08-04 21:29:33 -0300

    Add files via upload

---
+++ assets/js/game.js
@@ -0,0 +1,269 @@
+var pontuacaoAtual = 0
+
+function verificar1(){
+    let resposta1 = document.getElementById("correta1").checked
+    if (resposta1 == true) 
+    {
+        document.getElementById("span1").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span1").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span1").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span1").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar2(){
+    let resposta2 = document.getElementById("correta2").checked
+    if (resposta2 == true) 
+    {
+        document.getElementById("span2").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span2").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span2").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span2").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar3(){
+    let resposta3 = document.getElementById("correta3").checked
+    if (resposta3 == true) 
+    {
+        document.getElementById("span3").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span3").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span3").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span3").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+function verificar4(){
+    let resposta4= document.getElementById("correta4").checked
+    if (resposta4 == true) 
+    {
+        document.getElementById("span4").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span4").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span4").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span4").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar5(){
+    let resposta5= document.getElementById("correta5").checked
+    if (resposta5 == true) 
+    {
+        document.getElementById("span5").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span5").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span5").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span5").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+function verificar6(){
+    let resposta6= document.getElementById("correta6").checked
+    if (resposta6 == true) 
+    {
+        document.getElementById("span6").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span6").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span6").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span6").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar7(){
+    let resposta7= document.getElementById("correta7").checked
+    if (resposta7 == true) 
+    {
+        document.getElementById("span7").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span7").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span7").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span7").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar8(){
+    let resposta8 = document.getElementById("correta8").checked
+    if (resposta8 == true) 
+    {
+        document.getElementById("span8").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span8").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span8").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span8").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar9(){
+    let resposta9 = document.getElementById("correta9").checked
+    if (resposta9 == true) 
+    {
+        document.getElementById("span9").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span9").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span9").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span9").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+
+function verificar10(){
+    let resposta10 = document.getElementById("correta10").checked
+    if (resposta10 == true) 
+    {
+        document.getElementById("span10").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span10").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span10").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span10").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+
+function verificar11(){
+    let resposta11 = document.getElementById("correta11").checked
+    if (resposta11 == true) 
+    {
+        document.getElementById("span11").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span11").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span11").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span11").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function verificar12(){
+    let resposta12 = document.getElementById("correta12").checked
+    if (resposta12 == true) 
+    {
+        document.getElementById("span12").textContent = "Parabéns duelista, você acertou!!"
+        document.getElementById("span12").style.color = "green"
+        pontuacaoAtual += 20
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+    else
+    {
+        document.getElementById("span12").textContent = "Que pena duelista, você errou..."
+        document.getElementById("span12").style.color = "red"
+        pontuacaoAtual -= 10
+        document.getElementById("record").textContent = pontuacaoAtual
+
+        console.log(`Pontuação atual ${pontuacaoAtual}`)
+    }
+}
+
+function refresh(){
+    window.location.reload();
+}
---
+++ assets/js/ranking.js
@@ -0,0 +1,50 @@
+function saveRecord(){
+  var nick = prompt("Digite seu Nome: ");
+
+  let obj = { name: nick, points: record };
+  ranking.unshift(obj)
+
+  exibirRanking();
+
+  salvarRanking();
+}
+
+function salvarRanking(){
+  localStorage.setItem("ranking", JSON.stringify(ranking))
+}
+
+function exibirRanking(){   
+  function compare( a, b ) {
+      if ( a.points < b.points ){
+        return -10;
+      }
+      if ( a.points > b.points ){
+        return 20;
+      }
+      return 0;
+  }
+  ranking.sort(compare);
+  ranking.reverse();
+
+  
+  position = 1;
+  for(obj in ranking) {
+      obj = ranking[obj];
+      document.getElementById("nick_" + position).innerHTML = obj.name;
+      document.getElementById("points_" + position).innerHTML = obj.points;
+  }
+}
+
+function recuperarRanking(){
+  if(localStorage.getItem("ranking") == null){
+      return [];
+  }else{
+      let ranking = JSON.parse(localStorage.getItem("ranking"))
+      return ranking;
+  }
+}
+
+var pontuacaoAtual = 20
+var ranking = recuperarRanking();
+
+exibirRanking()
